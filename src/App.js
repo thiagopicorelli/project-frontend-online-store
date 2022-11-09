cebdd226814aa8fc9fe712defc5bd90e5f9d7b1e
hildelio <hildelio@gmail.com>
2022-11-09 17:17:29 -0300

    Requisito 3 Co-authored-by: Lucas Silva Silvestre <LucasSilvestre10@users.noreply.github.com> Co-authored-by: Thiago Picorelli <thiagopicorelli@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProductSearch from './Pages/ProductSearch';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={ ProductSearch } />
+        <Route exact path="/cart" component={ Cart } />
       </Switch>
     </Router>
   );
